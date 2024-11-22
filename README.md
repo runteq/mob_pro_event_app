b495b1b91ec8c7d0899e3b9cc9d0cc2227b98f3d
kenchasonakai <kenchasonakai@gmail.com>
2024-11-22 17:50:23 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+## 環境構築方法
+### 他で動いているコンテナがある場合があるので止めておきましょう
+※動作に関係無いことがわかっていれば止めなくとも構いません
+
+```
+docker stop $(docker ps -q)
+```
+
+### コンテナを立ち上げましょう
+```
+docker compose up
+```
+
+<details>
+<summary>問題2のゲームマスター用資料</summary>
+ゲームマスター以外の人は開かないでください
+https://github.com/kenchasonakai/mob_pro_gm/blob/main/%E5%95%8F%E9%A1%8C2.md
+</details>
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,65 @@
+class PostsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  def index
+    if params[:tag].present? && params[:tag].is_a?(String)
+      @posts = Post.tagged_with(params[:tag].strip).order(created_at: :desc)
+    else
+      @posts = Post.all.order(created_at: :desc)
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.html do
+        @post = current_user.posts.find(params[:id])
+      end
+
+      format.json do
+        post = Post.find(params[:id])
+        render json: { content: post.content }
+      end
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました"
+    else
+      flash.now[:alert] = "投稿に失敗しました\n#{@post.errors.full_messages.join('\n')}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "更新しました"
+    else
+      flash.now[:alert] = "更新に失敗しました\n#{@post.errors.full_messages.join('\n')}"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, notice: "削除しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :tag_list)
+  end
+end
